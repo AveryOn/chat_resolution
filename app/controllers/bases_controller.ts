e0efffb10b079e24c5db3769d6e71bdbe2b4252e
AveryOn <lnkonio2@gmail.com>
2024-05-12 01:04:23 +0300

    last-change

--- app/controllers/bases_controller.ts
+++ app/controllers/bases_controller.ts
@@ -1,17 +1,9 @@
 // import type { HttpContext } from '@adonisjs/core/http'
 
 import { HttpContext } from "@adonisjs/core/http";
-import db from "@adonisjs/lucid/services/db";
 
 export default class BasesController {
     async get({ response }: HttpContext) {
-
-        const rows = (await db.rawQuery(
-            `
-                SELECT * FROM test;
-
-            `
-        )).rows;
-        response.send({data: rows});
+        response.send({data: 'Hello World!'});
     }
 }
