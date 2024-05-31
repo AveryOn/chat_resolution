bb33aa07bff100173c44ee1e8e0a211c8bf4ca95
AveryOn <lnkonion2@gmail.com>
2024-05-31 16:22:31 +0300

    fix

--- app/controllers/auth_controller.ts
+++ app/controllers/auth_controller.ts
@@ -24,7 +24,6 @@ export default class AuthControllersController {
                     },
                 }
             }
-
             // Получение текущего пользователя по email
             const fetchedUser = await User.query().select('*').where('email', valideData.email).first();
 
